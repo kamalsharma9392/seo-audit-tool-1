deb15785736959723ba26aa1a91b371edcfa6d92
Aurelian <thefrozenone95@gmail.com>
2017-06-04 22:30:12 +0300

    added PDFGenerator, modified URLValidator

---
+++ Seo Audit Tool/Files/PdfGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.draw;
+using Seo_Audit_Tool.Analyzers;
+
+namespace Seo_Audit_Tool.Files
+{
+    public class PdfGenerator
+    {
+        public void GeneratePdfReport(string pageTitle, string pageUrl, Analyzer analyzer)
+        {
+            try
+            {
+                var date = DateTime.Now.ToString("dd-MM-yyyy hh-mm");
+                var fs = new FileStream($"{pageTitle} - {date}.pdf", FileMode.Create);
+                var document = new Document(PageSize.A4, 25, 25, 30, 30);
+                var writer = PdfWriter.GetInstance(document, fs);
+
+                document.AddAuthor("SEO Audit Tool");
+                document.AddKeywords("On-page SEO report");
+                document.AddTitle($"Report for: {pageUrl} {Environment.NewLine}");
+
+                document.Open();
+
+                var header = new Header("header", $"Report for: {pageUrl} {Environment.NewLine}Date: {DateTime.Now:dd-MM-yyyy hh:mm}");
+                document.Add(header);
+
+                var firstParagraph = new Paragraph("Search Engine Optimization Report");
+                firstParagraph.Alignment = 1;
+                firstParagraph.SpacingAfter = 10;
+                LineSeparator separator = new LineSeparator();
+
+                PdfPTable table = new PdfPTable(2);
+                table.SpacingBefore = 20;
+
+                table.AddCell("Keyword in title");
+                table.AddCell(analyzer.KeywordInTitle ? "Found" : "Not found");
+                table.AddCell("Keyword in description");
+                table.AddCell(analyzer.KeywordInDescription ? "Found" : "Not found");
+                table.AddCell("Keyword in headings");
+                table.AddCell(analyzer.KeywordInHeadings ? "Found" : "Not found");
+                table.AddCell("Keyword in URL");
+                table.AddCell(analyzer.KeywordInUrl ? "Found" : "Not found");
+
+                document.Add(firstParagraph);
+                document.Add(separator);
+                document.Add(table);
+
+                document.Close();
+                writer.Close();
+                fs.Close();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.StackTrace);
+            }
+        }
+    }
+}
